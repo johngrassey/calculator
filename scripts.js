5a28443e4e1273640c9ea6f2c422b38c2663e9ed
John Grassey <johngrassey@gmail.com>
2024-06-22 17:16:08 -0400

    Update functions to console and create variables
    Variables will store numbers entered in calculator.
    Using Console.log on functions for testing purposes.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
+let num1;
+let num2;
+let operator;
+
 function add(a,b) {
-    return a + b;
+    console.log( a + b );
 };
 
 function subtract(a,b) {
-    return a - b;
+    console.log( a - b );
 };
 
 function multiply(a,b) {
-    return a * b;
+    console.log( a * b );
 };
 
-function divite(a,b) {
-    return a / b;
+function divide(a,b) {
+    console.log( a / b );
 };
